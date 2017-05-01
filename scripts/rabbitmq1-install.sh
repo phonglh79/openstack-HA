2819ad630e9f2b354aed0a1ffb83ad015c167e17
CongTO <tcvn1985@gmail.com>
2017-05-01 19:11:12 +0700

    update script

--- scripts/rabbitmq1-install.sh
+++ scripts/rabbitmq1-install.sh
@@ -14,15 +14,8 @@ MQ2_HOSTNAME=mq2
 MQ3_HOSTNAME=mq3
 
 ### Kiem tra cu phap khi thuc hien shell 
-if [ $# -ne 1 ]; then
-        echo  "Cu phap dung nhu sau "
-        echo "Thuc hien tren may chu MQ1: bash $0 mq1"
-        echo "Thuc hien tren may chu MQ2: bash $0 mq2"
-        echo "Thuc hien tren may chu MQ3: bash $0 mq3"
-        exit 1;
-fi
 
-function copykey {
+function copykey() {
         ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ""
         for IP_ADD in $MQ1_IP_BOND1 $MQ2_IP_BOND1 $MQ3_IP_BOND1
         do
@@ -31,17 +24,17 @@ function copykey {
 }
 
 
-function install_proxy {
+function install_proxy() {
         echo "proxy=http://123.30.178.220:3142" >> /etc/yum.conf 
         yum -y update
 }
 
-function install_repo {
+function install_repo() {
         yum -y install centos-release-openstack-newton
         yum -y upgrade
 }
 
-function khai_bao_host {
+function khai_bao_host() {
                 echo "$MQ1_IP_BOND1 mq1" >> /etc/hosts
                 echo "$MQ2_IP_BOND1 mq2" >> /etc/hosts
                 echo "$MQ3_IP_BOND1 mq3" >> /etc/hosts
@@ -49,14 +42,14 @@ function khai_bao_host {
                 scp /etc/hosts root@$MQ3_IP_BOND1:/etc/
 }
 
-function install_rabbitmq {
+function install_rabbitmq() {
         yum -y install rabbitmq-server
         systemctl enable rabbitmq-server.service
         systemctl start rabbitmq-server.service
 }
 
 
-function config_rabbitmq {
+function config_rabbitmq() {
         rabbitmqctl add_user openstack Welcome123
         rabbitmqctl set_permissions openstack ".*" ".*" ".*"
         rabbitmqctl set_policy ha-all '^(?!amq\.).*' '{"ha-mode": "all"}'          
@@ -67,7 +60,7 @@ function config_rabbitmq {
 
 }
 
-function install_rabbitmq_join {
+function rabbitmq_join_cluster() {
         ssh root@$IP_ADD
         chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie
         chmod 400 /var/lib/rabbitmq/.erlang.cookie
@@ -76,7 +69,7 @@ function install_rabbitmq_join {
         rabbitmqctl stop_app
         rabbitmqctl join_cluster rabbit@$MQ1_HOSTNAME
         rabbitmqctl start_app
-        fi         
+     
 }
 
 ############################
@@ -97,12 +90,24 @@ echo "######################################"
 echo " install_proxy, install_repo "
 echo "######################################"
 sleep 5
-for IP_ADD for IP_ADD in $MQ1_IP_BOND1 $MQ2_IP_BOND1 $MQ3_IP_BOND1
+for IP_ADD in $MQ1_IP_BOND1 $MQ2_IP_BOND1 $MQ3_IP_BOND1
 do 
     ssh root@$IP_ADD "$(typeset -f); install_proxy"
     ssh root@$IP_ADD "$(typeset -f); install_repo"
     if [ "$IP_ADD" == "MQ1_IP_BOND1" ]; then 
       ssh root@$IP_ADD "$(typeset -f); khai_bao_host"
     fi 
-
+    
+    ssh root@$IP_ADD "$(typeset -f); install_rabbitmq"
+    
+    if [ "$IP_ADD" == "MQ1_IP_BOND1" ]; then 
+      ssh root@$IP_ADD "$(typeset -f); config_rabbitmq"
+    fi
+    
+    if [ "$IP_ADD" == "MQ2_IP_BOND1" || "$IP_ADD" == "MQ3_IP_BOND1"]; then
+      ssh root@$IP_ADD "$(typeset -f); rabbitmq_join_cluster"
+    fi 
 done 
+
+rabbitmqctl cluster_status
+echo done
