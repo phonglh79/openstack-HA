9c9d4b455b34527640fbdda31b696010e58799d3
CongTO <tcvn1985@gmail.com>
2017-05-10 11:28:46 +0700

    cai dat pacemaker
    cai dat pacemaker

--- scripts/lb-install.sh
+++ scripts/lb-install.sh
@@ -131,10 +131,13 @@ do
       echocolor "Cai dat install_nginx tren $IP_ADD"
       sleep 3
       ssh root@$IP_ADD "$(typeset -f); install_nginx"
-      
-      echocolor "Cai dat install_pacemaker_corosync tren $IP_ADD"
-      sleep 3
-      ssh root@$IP_ADD "$(typeset -f); install_pacemaker_corosync"      
+done 
+
+for IP_ADD in $LB1_IP_NIC3 $LB2_IP_NIC3
+do 
+echocolor "Cai dat install_pacemaker_corosync tren $IP_ADD"
+sleep 3
+ssh root@$IP_ADD "$(typeset -f); install_pacemaker_corosync"    
 done 
 
 echocolor "Done"
