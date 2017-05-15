534068958301059c079292732f893a3f3d0d0a0b
CongTO <tcvn1985@gmail.com>
2017-05-15 12:05:07 +0700

    cap nhat script add resources

--- scripts/lb-add-resources.sh
+++ scripts/lb-add-resources.sh
@@ -16,11 +16,12 @@ function echocolor {
 }
 
 function add_resources {
-        pcs resource create Virtual_IP ocf:heartbeat:IPaddr2 ip=$IP_VIP_ADMIN cidr_netmask=32 op monitor interval=30s
+        pcs resource create Virtual_IP_API ocf:heartbeat:IPaddr2 ip=$IP_VIP_API cidr_netmask=32 op monitor interval=30s
+        pcs resource create Virtual_IP_DB ocf:heartbeat:IPaddr2 ip=$IP_VIP_DB cidr_netmask=32 op monitor interval=30s
         pcs resource create Web_Cluster ocf:heartbeat:nginx configfile=/etc/nginx/nginx.conf status10url op monitor interval=5s
         # pcs resource clone Web_Cluster globally-unique=true clone-max=2 clone-node-max=2
-        pcs constraint colocation add Web_Cluster with Virtual_IP INFINITY        
-        pcs constraint order Virtual_IP then Web_Cluster
+        pcs constraint colocation add Web_Cluster with Virtual_IP_API INFINITY        
+        pcs constraint order Virtual_IP_API then Web_Cluster
 }
 
 #####
