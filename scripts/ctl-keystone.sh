7d54247f7f9bd3508fe728a3d19f7b3fc0e29330
CongTO <tcvn1985@gmail.com>
2017-05-21 00:22:57 +0700

    fix scrpt cai dat keystone
    fix scrpt cai dat keystone

--- scripts/ctl-keystone.sh
+++ scripts/ctl-keystone.sh
@@ -32,8 +32,8 @@ function ops_del {
 
 function create_keystone_db {
 mysql -uroot -p$PASS_DATABASE_ROOT -h $DB1_IP_NIC2 -e "CREATE DATABASE keystone;
-GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '"$PASS_DATABASE_KEYSTONE"';
-GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '"$PASS_DATABASE_KEYSTONE"';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$PASS_DATABASE_KEYSTONE';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$PASS_DATABASE_KEYSTONE';
 FLUSH PRIVILEGES;"
 }
 
@@ -43,17 +43,19 @@ function keystone_install {
 }
 
 function keystone_config {
-        /etc/keystone/keystone.conf=keystone_conf
+        keystone_conf=/etc/keystone/keystone.conf
         cp $keystone_conf $keystone_conf.orig        
-        ops_edit $keystone_conf database connection mysql+pymysql://keystone:$PASS_DATABASE_KEYSTONE@$virtual_ip/keystone
+        ops_edit $keystone_conf database connection mysql+pymysql://keystone:$PASS_DATABASE_KEYSTONE@$IP_VIP_API/keystone
         ops_edit $keystone_conf token provider fernet
         for IP_ADD in $CTL1_IP_NIC3 $CTL2_IP_NIC3 $CTL3_IP_NIC3
-        do
-                scp $keystone_conf root@$IP_ADD:/etc/keystone/
+        do  
+            scp $keystone_conf root@$IP_ADD:/etc/keystone/
+            scp -r /etc/keystone/credential-keys root@$IP_ADD:/etc/keystone/
+            scp -r /etc/keystone/fernet-keys root@$IP_ADD:/etc/keystone/
+            ssh root@$IP_ADD "chown keystone:keystone /etc/keystone/credential-keys/"
+            ssh root@$IP_ADD "chown keystone:keystone /etc/keystone/fernet-keys/"
         done
 }
-
-
 function keystone_create_fernet {
           su -s /bin/sh -c "keystone-manage db_sync" keystone
           keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
@@ -69,22 +71,27 @@ function keystone_bootstrap {
 }
 
 function keystone_config_http {
-          echo "ServerName `hostname`" >> /etc/httpd/conf/httpd.conf
-          ln -s /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
-          systemctl enable httpd.service
-          systemctl start httpd.service
-}
+        for IP_ADD in $CTL1_IP_NIC3 $CTL2_IP_NIC3 $CTL3_IP_NIC3
+        do  
+        ssh root@$IP_ADD << EOF               
+echo "ServerName `hostname`" >> /etc/httpd/conf/httpd.conf
+ln -s /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
+systemctl enable httpd.service
+systemctl start httpd.service
+EOF
+        done       
 
+}
 function keystone_create_adminrc {
-            echo "OS_USERNAME=admin" > /root/adminrc
-            echo "OS_PASSWORD=ADMIN_PASS" >> /root/adminrc
-            echo "OS_PROJECT_NAME=admin"  >> /root/adminrc
-            echo "OS_USER_DOMAIN_NAME=Default"  >> /root/adminrc
-            echo "OS_PROJECT_DOMAIN_NAME=Default"  >> /root/adminrc
-            echo "OS_AUTH_URL=http://$IP_VIP_API:35357/v3" >> /root/adminrc
-            echo "OS_IDENTITY_API_VERSION=3" >> /root/adminrc
+            echo "export OS_USERNAME=admin" > /root/adminrc
+            echo "export OS_PASSWORD=$ADMIN_PASS" >> /root/adminrc
+            echo "export OS_PROJECT_NAME=admin"  >> /root/adminrc
+            echo "export OS_USER_DOMAIN_NAME=Default"  >> /root/adminrc
+            echo "export OS_PROJECT_DOMAIN_NAME=Default"  >> /root/adminrc
+            echo "export OS_AUTH_URL=http://$IP_VIP_API:35357/v3" >> /root/adminrc
+            echo "export OS_IDENTITY_API_VERSION=3" >> /root/adminrc
+            chmod +x /root/adminrc
 }
-
 
 ############################
 # Thuc thi cac functions
@@ -101,7 +108,7 @@ echocolor "Cai dat keystone"
 sleep 3
 for IP_ADD in $CTL1_IP_NIC3 $CTL2_IP_NIC3 $CTL3_IP_NIC3
 do
-    echocolor "Cai dat proxy tren $IP_ADD"
+    echocolor "Cai dat keystone_install $IP_ADD"
     sleep 3
     ssh root@$IP_ADD "$(typeset -f); keystone_install"
 done
