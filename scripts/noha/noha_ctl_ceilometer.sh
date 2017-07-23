a63c14cffcfd46dd21bae7acaa15585cc1d5e79d
Cong To <tcvn1985@gmail.com>
2017-07-23 17:21:05 +0700

    dinh dang script cho dep

--- scripts/noha/noha_ctl_ceilometer.sh
+++ scripts/noha/noha_ctl_ceilometer.sh
@@ -30,83 +30,83 @@ function ops_del {
 }
 
 function aodh_create_db {
-      mysql -uroot -p$PASS_DATABASE_ROOT  -e "CREATE DATABASE aodh;
-      GRANT ALL PRIVILEGES ON aodh.* TO 'aodh'@'localhost' IDENTIFIED BY '$PASS_DATABASE_AODH' WITH GRANT OPTION ;FLUSH PRIVILEGES;
-      GRANT ALL PRIVILEGES ON aodh.* TO 'aodh'@'%' IDENTIFIED BY '$PASS_DATABASE_AODH' WITH GRANT OPTION ;FLUSH PRIVILEGES;
-      GRANT ALL PRIVILEGES ON aodh.* TO 'aodh'@'$CTL1_IP_NIC1' IDENTIFIED BY '$PASS_DATABASE_AODH' WITH GRANT OPTION ;FLUSH PRIVILEGES;
+		mysql -uroot -p$PASS_DATABASE_ROOT  -e "CREATE DATABASE aodh;
+		GRANT ALL PRIVILEGES ON aodh.* TO 'aodh'@'localhost' IDENTIFIED BY '$PASS_DATABASE_AODH' WITH GRANT OPTION ;FLUSH PRIVILEGES;
+		GRANT ALL PRIVILEGES ON aodh.* TO 'aodh'@'%' IDENTIFIED BY '$PASS_DATABASE_AODH' WITH GRANT OPTION ;FLUSH PRIVILEGES;
+		GRANT ALL PRIVILEGES ON aodh.* TO 'aodh'@'$CTL1_IP_NIC1' IDENTIFIED BY '$PASS_DATABASE_AODH' WITH GRANT OPTION ;FLUSH PRIVILEGES;
 
-      FLUSH PRIVILEGES;"
+		FLUSH PRIVILEGES;"
 }
 
 function aodh_user_endpoint {
-			openstack user create aodh --domain default --password $AODH_PASS
-			openstack role add --project service --user aodh admin
-			
-			openstack service create --name aodh --description "Telemetry" alarming
-			openstack endpoint create --region RegionOne alarming public http://$CTL1_IP_NIC1:8042
-			openstack endpoint create --region RegionOne alarming internal http://$CTL1_IP_NIC1:8042
-			openstack endpoint create --region RegionOne alarming admin http://$CTL1_IP_NIC1:8042
+		openstack user create aodh --domain default --password $AODH_PASS
+		openstack role add --project service --user aodh admin
+		
+		openstack service create --name aodh --description "Telemetry" alarming
+		openstack endpoint create --region RegionOne alarming public http://$CTL1_IP_NIC1:8042
+		openstack endpoint create --region RegionOne alarming internal http://$CTL1_IP_NIC1:8042
+		openstack endpoint create --region RegionOne alarming admin http://$CTL1_IP_NIC1:8042
 }
 
 function aodh_install_config {
-			echocolor "Cai dat AODH"
-			sleep 3
-			yum -y install openstack-aodh-api \
-			openstack-aodh-evaluator openstack-aodh-notifier \
-			openstack-aodh-listener openstack-aodh-expirer \
-			python-aodhclient
-			
-			yum -y install mod_wsgi memcached python-memcached httpd install python-pip
-			
-			pip install requests-aws
-			
-			ctl_aodh_conf=/etc/aodh/aodh.conf
-			cp $ctl_aodh_conf $ctl_aodh_conf.orig
+		echocolor "Cai dat AODH"
+		sleep 3
+		yum -y install openstack-aodh-api \
+		openstack-aodh-evaluator openstack-aodh-notifier \
+		openstack-aodh-listener openstack-aodh-expirer \
+		python-aodhclient
+		
+		yum -y install mod_wsgi memcached python-memcached httpd install python-pip
+		
+		pip install requests-aws
+		
+		ctl_aodh_conf=/etc/aodh/aodh.conf
+		cp $ctl_aodh_conf $ctl_aodh_conf.orig
 
-			ops_edit $ctl_aodh_conf DEFAULT rpc_backend rabbit
-			ops_edit $ctl_aodh_conf DEFAULT auth_strategy keystone
-			ops_edit $ctl_aodh_conf DEFAULT my_ip $CTL1_IP_NIC1
-			ops_edit $ctl_aodh_conf DEFAULT host `hostname`
-			
-			
-			ops_edit $ctl_aodh_conf database connection  mysql+pymysql://aodh:$PASS_DATABASE_AODH@$CTL1_IP_NIC1/aodh
+		ops_edit $ctl_aodh_conf DEFAULT rpc_backend rabbit
+		ops_edit $ctl_aodh_conf DEFAULT auth_strategy keystone
+		ops_edit $ctl_aodh_conf DEFAULT my_ip $CTL1_IP_NIC1
+		ops_edit $ctl_aodh_conf DEFAULT host `hostname`
+		
+		
+		ops_edit $ctl_aodh_conf database connection  mysql+pymysql://aodh:$PASS_DATABASE_AODH@$CTL1_IP_NIC1/aodh
 
-			ops_edit $ctl_aodh_conf keystone_authtoken auth_uri http://$CTL1_IP_NIC1:5000
-			ops_edit $ctl_aodh_conf keystone_authtoken auth_url http://$CTL1_IP_NIC1:35357
-			ops_edit $ctl_aodh_conf keystone_authtoken memcached_servers $CTL1_IP_NIC1:11211
-			ops_edit $ctl_aodh_conf keystone_authtoken auth_type password
-			ops_edit $ctl_aodh_conf keystone_authtoken project_domain_name Default
-			ops_edit $ctl_aodh_conf keystone_authtoken user_domain_name Default
-			ops_edit $ctl_aodh_conf keystone_authtoken project_name service
-			ops_edit $ctl_aodh_conf keystone_authtoken username aodh
-			ops_edit $ctl_aodh_conf keystone_authtoken password $AODH_PASS
-			
-			ops_edit $ctl_aodh_conf oslo_messaging_rabbit rabbit_host $CTL1_IP_NIC1
-			ops_edit $ctl_aodh_conf oslo_messaging_rabbit rabbit_port 5672
-			ops_edit $ctl_aodh_conf oslo_messaging_rabbit rabbit_userid openstack
-			ops_edit $ctl_aodh_conf oslo_messaging_rabbit rabbit_password $RABBIT_PASS
-			
-			ops_edit $ctl_aodh_conf service_credentials auth_type password
-			ops_edit $ctl_aodh_conf service_credentials auth_url http://$CTL1_IP_NIC1:5000/v3
-			ops_edit $ctl_aodh_conf service_credentials project_domain_name default
-			ops_edit $ctl_aodh_conf service_credentials user_domain_name default
-			ops_edit $ctl_aodh_conf service_credentials project_name service
-			ops_edit $ctl_aodh_conf service_credentials username aodh
-			ops_edit $ctl_aodh_conf service_credentials password $AODH_PASS
-			ops_edit $ctl_aodh_conf service_credentials interface internalURL
-			ops_edit $ctl_aodh_conf service_credentials region_name RegionOne
-			
-			ops_edit $ctl_aodh_conf api port 8042
-			ops_edit $ctl_aodh_conf api host 0.0.0.0
-			ops_edit $ctl_aodh_conf api paste_config api_paste.ini
-			
-			ops_edit $ctl_aodh_conf oslo_messaging_notifications driver messagingv2
-			ops_edit $ctl_aodh_conf oslo_messaging_notifications topics notifications
+		ops_edit $ctl_aodh_conf keystone_authtoken auth_uri http://$CTL1_IP_NIC1:5000
+		ops_edit $ctl_aodh_conf keystone_authtoken auth_url http://$CTL1_IP_NIC1:35357
+		ops_edit $ctl_aodh_conf keystone_authtoken memcached_servers $CTL1_IP_NIC1:11211
+		ops_edit $ctl_aodh_conf keystone_authtoken auth_type password
+		ops_edit $ctl_aodh_conf keystone_authtoken project_domain_name Default
+		ops_edit $ctl_aodh_conf keystone_authtoken user_domain_name Default
+		ops_edit $ctl_aodh_conf keystone_authtoken project_name service
+		ops_edit $ctl_aodh_conf keystone_authtoken username aodh
+		ops_edit $ctl_aodh_conf keystone_authtoken password $AODH_PASS
+		
+		ops_edit $ctl_aodh_conf oslo_messaging_rabbit rabbit_host $CTL1_IP_NIC1
+		ops_edit $ctl_aodh_conf oslo_messaging_rabbit rabbit_port 5672
+		ops_edit $ctl_aodh_conf oslo_messaging_rabbit rabbit_userid openstack
+		ops_edit $ctl_aodh_conf oslo_messaging_rabbit rabbit_password $RABBIT_PASS
+		
+		ops_edit $ctl_aodh_conf service_credentials auth_type password
+		ops_edit $ctl_aodh_conf service_credentials auth_url http://$CTL1_IP_NIC1:5000/v3
+		ops_edit $ctl_aodh_conf service_credentials project_domain_name default
+		ops_edit $ctl_aodh_conf service_credentials user_domain_name default
+		ops_edit $ctl_aodh_conf service_credentials project_name service
+		ops_edit $ctl_aodh_conf service_credentials username aodh
+		ops_edit $ctl_aodh_conf service_credentials password $AODH_PASS
+		ops_edit $ctl_aodh_conf service_credentials interface internalURL
+		ops_edit $ctl_aodh_conf service_credentials region_name RegionOne
+		
+		ops_edit $ctl_aodh_conf api port 8042
+		ops_edit $ctl_aodh_conf api host 0.0.0.0
+		ops_edit $ctl_aodh_conf api paste_config api_paste.ini
+		
+		ops_edit $ctl_aodh_conf oslo_messaging_notifications driver messagingv2
+		ops_edit $ctl_aodh_conf oslo_messaging_notifications topics notifications
 
 }
 
 function aodh_syncdb {
-		 aodh-dbsync --config-dir /etc/aodh/
+		aodh-dbsync --config-dir /etc/aodh/
 
 }
 
@@ -125,7 +125,6 @@ function aodh_wsgi_config {
 }
 
 function aodh_enable_restart {
-
 		echocolor "Restart dich vu aodh"
 		sleep 3
 		systemctl stop openstack-aodh-api.service 
@@ -140,8 +139,6 @@ function aodh_enable_restart {
 		openstack-aodh-evaluator.service \
 		openstack-aodh-notifier.service \
 		openstack-aodh-listener.service
-
-
 }
 
 
